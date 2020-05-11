54c5822606481b43b40d60eaae06b598cafe425a
Jeffrey-0 <632539887@qq.com>
2020-05-11 11:37:23 +0800

    test-servlet

--- src/servlet/ClassilyServlet.java
+++ src/servlet/ClassilyServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+// test
 @WebServlet("/classily")
 public class ClassilyServlet extends HttpServlet {
     ResDao rd = new ResDao();
